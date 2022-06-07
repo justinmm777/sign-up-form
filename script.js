e66001c07e439d99f5c32ec8a3a21335ff8aaa09
Justin <justinmm777@gmail.com>
2022-06-08 00:16:06 +0100

    add input field validation

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+// blur event for all input fields in form
+const inputFields = document.querySelectorAll('#signUpForm input');
+
+inputFields.forEach((element) => {
+    element.addEventListener('blur', function() {
+        //if input field passes validation remove the error class, else add it
+            if(this.checkValidity()) {
+            element.classList.remove('error-input');
+            }
+            else {
+                element.classList.add('error-input');
+            }
+        }
+    )
+});
